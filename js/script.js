30785fbedcddd3bf16eb668251c27af5c4dc3b8e
HarllonCS <harllon.santana@gmail.com>
2023-03-13 23:40:41 -0300

    JavaScript simple

--- js/script.js
+++ js/script.js
@@ -1,53 +1,15 @@
-var divLogin = document.querySelector('div.container-login')
+var bd = document.body
+var divLogin = document.querySelector('#cont-login')
+var divSignUp = document.querySelector('#cont-signup')
 
-var divSignUp = document.createElement('div')
-divSignUp.setAttribute('class', 'container-login')
+// bd.removeChild(divSignUp)
 
-var header = document.createElement('header')
-var h1 = document.createElement('h1')
-var form = document.createElement('form')
+document.querySelector('#switch-login').addEventListener('click', () => {
+    bd.removeChild(divLogin)
+    bd.appendChild(divSignUp)
+})
 
-var divFormData = [
-    document.createElement('div'),
-    document.createElement('div'),
-    document.createElement('div'),
-    document.createElement('div')
-]
-for (i in divFormData) {divFormData[i].setAttribute('class', 'form-data')}
-
-var lblName = document.createElement('label')
-lblName.setAttribute('for', 'nameSignUp')
-lblName.innerText = 'Username'
-
-var inpName = document.createElement('input')
-inpName.setAttribute('type', 'text')
-inpName.setAttribute('id', 'nameSignUp')
-inpName.setAttribute('required', '')
-inpName.setAttribute('autofocus', '')
-inpName.setAttribute('placeholder', "It doesn't have to be your real name.")
-
-var lblEmail = document.createElement('label')
-lblEmail.setAttribute('for', 'emailSignUp')
-lblEmail.innerText = 'E-mail'
-
-var divDataFlex = document.createElement('div')
-divDataFlex.setAttribute('class', 'data-flex')
-
-var lblPass = document.createElement('label')
-lblPass.setAttribute('for', 'passSignUp1')
-lblPass.innerText = 'Password'
-
-var lblConfPass = document.createElement('label')
-lblConfPass.setAttribute('for', 'passSignUp2')
-lblConfPass.innerText = 'Repeat Password'
-
-var divFormLinks = document.createElement('div')
-divFormLinks.setAttribute('class', 'form-links')
-
-var btn = document.createElement('button')
-btn.setAttribute('type', 'submit')
-btn.innerText = 'create'
-
-var p = document.createElement('p')
-p.innerText = 'sign in'
-
+document.querySelector('#switch-signup').addEventListener('click', () => {
+    bd.removeChild(divSignUp)
+    bd.appendChild(divLogin)
+})

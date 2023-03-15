a7b1a4a7a07e551fb2be9e4b887692c4afe42630
HarllonCS <harllon.santana@gmail.com>
2023-03-14 22:38:50 -0300

    With an error, but working

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ var bd = document.body
 var divLogin = document.querySelector('#cont-login')
 var divSignUp = document.querySelector('#cont-signup')
 
-// bd.removeChild(divSignUp)
+bd.removeChild(divSignUp)
 
 document.querySelector('#switch-login').addEventListener('click', () => {
     bd.removeChild(divLogin)
     bd.appendChild(divSignUp)
-})
 
-document.querySelector('#switch-signup').addEventListener('click', () => {
-    bd.removeChild(divSignUp)
-    bd.appendChild(divLogin)
+    document.querySelector('#switch-signup').addEventListener('click', () => {
+        bd.removeChild(divSignUp)
+        bd.appendChild(divLogin)
+    })
 })
